5e53fc4c6d94415ef9000c41b779d0775f158b73
cheongha <sch7188@gmail.com>
2022-06-24 21:24:23 +0900

    feat: get shop
    가게 리스트 조회

--- order/views.py
+++ order/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from order.models import Shop, Menu, Order, OrderFood
+from order.serializers import ShopSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+@csrf_exempt
+def shop(request):
+    if request.method == 'GET':
+        shop = Shop.objects.all()
+        serializer = ShopSerializer(shop, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    # elif request.method == 'POST':
+    #     data = JSONParser().parse(request)
+    #     serializer = ShopSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(serializer.data, status=201)
+    #     return JsonResponse(serializer.errors, status=400)
