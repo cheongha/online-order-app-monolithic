e0e6c3954a66918b3265cb2c71a09ef203d561bd
cheongha <sch7188@gmail.com>
2022-06-24 21:25:52 +0900

    chore: add order model to admin
    Shop, Menu, Order, OrderFood 관리자 페이지에 추가

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,10 @@
+from ast import Or
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shop, Menu, Order, OrderFood
+
+admin.site.register(Shop)
+admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(OrderFood)
+
